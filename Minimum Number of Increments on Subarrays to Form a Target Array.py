fbd8a1d8fa4c0e1455a827f84d44ce0342b5f001
mahfuz-ad99 <mafi6470@gmail.com>
2026-06-19 11:03:00 -0700

    Create Minimum Number of Increments on Subarrays to Form a Target Array.py

---
+++ Minimum Number of Increments on Subarrays to Form a Target Array.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def minNumberOperations(self, target: List[int]) -> int:
+
+      
+        answer = target[0]
+
+        for i in range(1, len(target)):
+
+            
+            if target[i] > target[i - 1]:
+                answer += target[i] - target[i - 1]
+
+        return answer
